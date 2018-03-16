f1edcb5e9ee36f8e6bff2868c7b4b07e3cc64a34
wcardoso4k <wellington.richard.cardoso@gmail.com>
2018-03-15 21:41:48 -0300

    Ajustes finais

--- WebApplication6/WebApplication6.Tests/Controllers/HomeControllerTest.cs
+++ WebApplication6/WebApplication6.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebApplication6;
 using WebApplication6.Controllers;
+using WebApplication6.db;
+using WebApplication6.Models;
 
 namespace WebApplication6.Tests.Controllers
 {
@@ -13,16 +14,64 @@ namespace WebApplication6.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public async Task Index()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
-            //ViewResult result = (ViewResult) controller.Index();
+            var result = await controller.Index();
 
             // Assert
-            //Assert.IsNotNull(result);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void SaveRepositorys()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            GiHubContext context = new GiHubContext();
+
+            context.Database.CreateIfNotExists();
+
+            context.Repositories.Add(new RepositoryGitHub
+            {
+                Name =  "repo",
+                Created = DateTimeOffset.Now,
+                CloneUrl = "url/test",
+                Description = "Description 123",
+                ForksCount = 1,
+                HtmlUrl = "url/test2222"
+            });
+            context.SaveChanges();
+        }
+
+        [TestMethod]
+        public void GetAllLanguages()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            var languages =  controller.GetAllLanguages();
+
+            Assert.IsNotNull(languages);
+        }
+
+        [TestMethod]
+        public void GetRepositoriesAzure()
+        {
+            // Arrange
+            var context = new GiHubContext();
+
+            Assert.IsTrue(context.Repositories.Any());
+
+            foreach (var repository in context.Repositories)
+            {
+                Debug.WriteLine("Id : {0}", repository.Id);
+            }
         }
     }
 }
--- WebApplication6/WebApplication6/App_Start/BundleConfig.cs
+++ WebApplication6/WebApplication6/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace WebApplication6
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/gitHub").Include(
+                "~/Scripts/gitHub.js*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,16 @@ namespace WebApplication6
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            #region EnableOptimizations
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+#if !DEBUG
+            BundleTable.EnableOptimizations = true;
+#endif
+
+            #endregion
         }
     }
 }
