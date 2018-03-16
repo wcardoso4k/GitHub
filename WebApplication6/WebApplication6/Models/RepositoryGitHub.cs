357c8c0906063abc36259d77988281fa711dee3d
wcardoso4k <wellington.richard.cardoso@gmail.com>
2018-03-15 21:43:22 -0300

    ..
    ..

---
+++ WebApplication6/WebApplication6/Models/RepositoryGitHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication6.Models
+{
+    public class RepositoryGitHub
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ForksCount { get; set; }
+        public string CloneUrl { get; set; }
+        public string HtmlUrl { get; set; }
+        public DateTimeOffset Created { get; set; }
+    }
+}
---
+++ WebApplication6/WebApplication6/db/GiHubContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using WebApplication6.Models;
+
+namespace WebApplication6.db
+{
+    public class GiHubContext : DbContext
+    {
+        public GiHubContext()
+            : base("dbAzure")
+        {
+        }
+
+        public DbSet<RepositoryGitHub> Repositories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
